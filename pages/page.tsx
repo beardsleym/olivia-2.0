11c18de129c50870eca587a5587769179e4ebe15
beardsleym <matthew@beardsley.com.au>
2022-11-28 14:28:25 -0500

    add chakraUI

---
+++ pages/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
+        </h1>
+        <Button>test</Button>
+
+        <p>
+          Get started by editing <code>app/page.tsx</code>
+        </p>
+      </main>
+    </div>
+  );
+}
---
+++ styles/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: {
+    50: "#ffe6ea",
+    100: "#f6bfc5",
+    200: "#e9979e",
+    300: "#de6d79",
+    400: "#d44553",
+    500: "#ba2b39",
+    600: "#92212d",
+    700: "#69161f",
+    800: "#410a11",
+    900: "#1d0103",
+  },
+};
+
+export const theme = extendTheme({ colors });
