598d918c20b2674035b258f067c9ffb8d2173eca
beardsleym <matthew@beardsley.com.au>
2022-11-28 15:17:45 -0500

    add embla carousell

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
+import { Carousel } from "../components/Carousel";
 
 export default function Home() {
-  return <Box>hello</Box>;
+  const slides = [
+    "headshots/Olivia-Beardsley-9058.jpg",
+    "headshots/Olivia-Beardsley-8798.jpg",
+  ];
+
+  return (
+    <Box>
+      <Carousel slides={slides} />
+      <Heading variant="h1">Olivia Beardsley</Heading>
+      <Heading variant="h1">Actor / Voice Artist / Singer</Heading>
+    </Box>
+  );
 }
