b8da67a5ea2fd71ad5915b99bea86be5b0496674
beardsleym <matthew@beardsley.com.au>
2022-11-29 19:54:46 -0500

    attempt to fix color mode

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,8 +1,7 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  useSystemColorMode: true,
-  initialColorMode: "light",
+  initialColorMode: "system",
 };
 
 const colors = {
